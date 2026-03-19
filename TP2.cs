b9cd97dc97c26561846ca8ba29d156f51cd01344
RamirezKevin9 <kevin.ramirezet32@gmail.com>
2026-03-19 11:17:27 -0300

    Add files via upload

---
+++ TP2.cs
@@ -0,0 +1,172 @@
+﻿if (25 > 30)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (14 <= 14)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (0 == 0) 
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (10 != 10)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (8 >= 3)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (15 < 15)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (100 != 50)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (7 == 7.0)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (0.5 > 0)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+if (-5 < -2)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+if (5 > 3 && 10 < 20) {
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion"); }
+
+if (8 == 10  ||  5 != 2)
+{
+        Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+    }
+
+if (10 != 10) { 
+Console.WriteLine("Es verdadera");
+}
+else
+{
+    Console.WriteLine("Es falsa la afirmacion");
+    }
+
+if (15 > 10 && !(5 > 10)) {
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+    }
+
+if (10 + 2 > 10 || 5 * 2 < 8) {
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+    }
+
+int temperatura = 35;
+bool humedad = false;
+
+    if (temperatura > 30 && humedad == true) {
+        Console.WriteLine("Es verdadera la afirmacion");
+    }
+    else
+    {
+        Console.WriteLine("Es Falsa la afirmacion");
+    }
+
+int edad = 16;
+    if(!(edad>= 18)) {
+        Console.WriteLine("Es verdadera la afirmacion");
+    }
+    else
+    {
+        Console.WriteLine("Es Falsa la afirmacion");
+    }
+
+if (10 / 3 == 3 && 10 % 3 == 1) {
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+
+string sin_nombre = "ana";
+if (sin_nombre == "juan" || sin_nombre == "ana") {
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
+}
+if ((5 * 2) + 10 > 15 && 20 / 4 == 5)
+{
+    Console.WriteLine("Es verdadera la afirmacion");
+}
+else
+{
+    Console.WriteLine("Es Falsa la afirmacion");
