21d179cc1662ea1b6401f585a01f6bc540c5320e
RamirezKevin9 <kevin.ramirezet32@gmail.com>
2026-04-07 13:12:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Ingrese su nombre");
+    string nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su promedio");
+    float promedio = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Ingrese la distancia en kilómetros desde su casa a la universidad");
+   int distancia = Convert.ToInt32(Console.ReadLine());
+
+bool DeterminarBeca (float promedio, int distancia)
+{
+    if (promedio >= 8.5 || distancia > 50)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool resultado = DeterminarBeca(promedio, distancia);
+if (resultado == true)
+{
+    Console.Write("Felicidades, ");
+    Console.Write(nombre);
+    Console.Write(" tu beca ha sido aprobada.");
+}
+else
+{
+    Console.Write("Lo sentimos,");
+    Console.Write(nombre);
+    Console.Write(" no cumples con los requisitos mínimos.");
+}
+
