fb9375deb1ffc4ae29fc5cffff80b96e3b66bdff
RamirezKevin9 <kevin.ramirezet32@gmail.com>
2026-04-30 12:02:53 -0300

    Add files via upload

---
+++ tp 10.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ingrese un número de dos cifras:");
+int numero = int.Parse(Console.ReadLine());
+
+int unidades = numero % 10;
+int decenas = numero / 10;
+
+Console.WriteLine("Cifra de las unidades: " + unidades);
+Console.WriteLine("Cifra de las decenas: " + decenas);  
