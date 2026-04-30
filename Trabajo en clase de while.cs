af51f5a79fcadf3408ec925d72ff8ca5171c3e10
RamirezKevin9 <kevin.ramirezet32@gmail.com>
2026-04-30 09:06:49 -0300

    Add files via upload

---
+++ Trabajo en clase de while.cs
@@ -0,0 +1,19 @@
+﻿// ejercicio 1
+
+Console.Write("Ingrese 0 o 1, si se ingresa otro número, pedir otra vez: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+while (numero != 0 && numero != 1)
+{
+    Console.Write("Número inválido. Ingrese 0 o 1: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+}
+
+// ejercicio 2
+
+Console.Write("Ingrese un número de 2 dígitos: ");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+while (numero2 < 10 || numero2 > 99)
+{
+    Console.Write("Número inválido. Ingrese un número de 2 dígitos: ");
+    numero2 = Convert.ToInt32(Console.ReadLine());
+}
