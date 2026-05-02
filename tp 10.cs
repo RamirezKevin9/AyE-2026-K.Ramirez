e3845294f4437c93d731c62b3b30837743872791
RamirezKevin9 <kevin.ramirezet32@gmail.com>
2026-05-02 15:41:35 -0300

    Delete tp 10.cs

--- tp 10.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Ingrese un número de dos cifras:");
-int numero = int.Parse(Console.ReadLine());
-
-int unidades = numero % 10;
-int decenas = numero / 10;
-
-Console.WriteLine("Cifra de las unidades: " + unidades);
-Console.WriteLine("Cifra de las decenas: " + decenas);  
