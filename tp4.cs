df9d35e3958d9a301781aebc2f24d1705c094694
RamirezKevin9 <kevin.ramirezet32@gmail.com>
2026-05-09 22:25:52 -0300

    Add files via upload

---
+++ tp4.cs
@@ -0,0 +1,36 @@
+﻿// Ejercicio 1
+Console.WriteLine("Ejercicio 1");
+for (int x = 0; x < 11; x++)
+{
+    Console.WriteLine(x);
+}
+
+// Ejercicio 2
+Console.WriteLine("Ejercicio 2");
+for (int x = 0; x < 5; x++)
+{
+    Console.WriteLine("Hola Mundo");
+}
+
+// Ejercicio 3
+Console.WriteLine("Ejercicio 3");
+for (int x = 2; x <= 20; x+=2)
+{
+    Console.WriteLine(x);
+}
+
+// Ejercicio 4
+Console.WriteLine("Ejercicio 4");
+for (int x = 7; x <= 70; x+=7)
+{
+    Console.WriteLine(x);
+}
+
+// Ejercicio 5
+Console.WriteLine("Ejercicio 5");
+int suma = 0;
+for (int i = 1; i <= 5; i++)
+{
+    suma += i;
+    Console.WriteLine(suma);
+}
