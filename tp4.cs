c5c52b5430d010ff743b938a89e28179df7b214a
RamirezKevin9 <kevin.ramirezet32@gmail.com>
2026-05-09 22:27:01 -0300

    Update tp4.cs

--- tp4.cs
+++ tp4.cs
@@ -1,36 +1,74 @@
-﻿// Ejercicio 1
+// Ejercicio 1
 Console.WriteLine("Ejercicio 1");
-for (int x = 0; x < 11; x++)
+
+void MostrarNumeros(int x)
 {
-    Console.WriteLine(x);
+    if (x <= 10)
+    {
+        Console.WriteLine(x);
+        MostrarNumeros(x + 1);
+    }
 }
+
+MostrarNumeros(0);
+
 
 // Ejercicio 2
 Console.WriteLine("Ejercicio 2");
-for (int x = 0; x < 5; x++)
+
+void HolaMundo(int x)
 {
-    Console.WriteLine("Hola Mundo");
+    if (x < 5)
+    {
+        Console.WriteLine("Hola Mundo");
+        HolaMundo(x + 1);
+    }
 }
+
+HolaMundo(0);
+
 
 // Ejercicio 3
 Console.WriteLine("Ejercicio 3");
-for (int x = 2; x <= 20; x+=2)
+
+void Pares(int x)
 {
-    Console.WriteLine(x);
+    if (x <= 20)
+    {
+        Console.WriteLine(x);
+        Pares(x + 2);
+    }
 }
+
+Pares(2);
+
 
 // Ejercicio 4
 Console.WriteLine("Ejercicio 4");
-for (int x = 7; x <= 70; x+=7)
+
+void MultiplosDe7(int x)
 {
-    Console.WriteLine(x);
+    if (x <= 70)
+    {
+        Console.WriteLine(x);
+        MultiplosDe7(x + 7);
+    }
 }
+
+MultiplosDe7(7);
+
 
 // Ejercicio 5
 Console.WriteLine("Ejercicio 5");
-int suma = 0;
-for (int i = 1; i <= 5; i++)
+
+void SumaRecursiva(int numero, int suma)
 {
-    suma += i;
-    Console.WriteLine(suma);
+    if (numero <= 5)
+    {
+        suma += numero;
+        Console.WriteLine(suma);
+        SumaRecursiva(numero + 1, suma);
+    }
 }
+
+SumaRecursiva(1, 0);
